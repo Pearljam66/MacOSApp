e7e36913d80483f097d4c686ded36d1f49860ff1
Sarah Clark <sarahireneclark@gmail.com>
2023-11-10 10:54:48 -0600

    Added list categories and search field

--- MacOSApp/MacOSApp/Views/MainView.swift
+++ MacOSApp/MacOSApp/Views/MainView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct MainView: View {
+    @State private var searchText = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                Label("Discover", systemImage: "star")
+                    .onTapGesture {
+                        print("Discover")
+                    }
+                Label("Arcade", systemImage: "arcade.stick")
+                    .onTapGesture {
+                        print("Arcade")
+                    }
+                Label("Create", systemImage: "paintbrush")
+                    .onTapGesture {
+                        print("Create")
+                    }
+                Label("Work", systemImage: "location")
+                    .onTapGesture {
+                        print("Work")
+                    }
+                Label("Play", systemImage: "gamecontroller")
+                    .onTapGesture {
+                        print("Play")
+                    }
+                Label("Develop", systemImage: "hammer")
+                    .onTapGesture {
+                        print("Develop")
+                    }
+                Label("Categories", systemImage: "squareshape.split.2x2")
+                    .onTapGesture {
+                        print("Categories")
+                    }
+                Label("Updates", systemImage: "square.and.arrow.down")
+                    .onTapGesture {
+                        print("Updates")
+                    }
+            }
+            .searchable(text: $searchText)
+            .onSubmit (of: .search) {
+                print(searchText)
+            }
         }
-        .padding()
     }
 }
 
