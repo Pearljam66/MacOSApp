b39db4daa847fea215c2d465c2f76f6ccd0b99b2
Sarah Clark <sarahireneclark@gmail.com>
2023-11-09 20:01:54 -0600

    Initial commit of xcode project

---
+++ MacOSApp/MacOSApp/MacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOSAppApp.swift
+//  MacOSApp
+//
+//  Created by Sarah Clark on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
